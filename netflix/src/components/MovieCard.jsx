c6c6141e2da489e019f16604efbac65ecb3a2441
dipakaml <dipakamzare56432@gmail.com>
2026-02-19 15:55:04 +0530

    background changes in MovieCard

--- netflix/src/components/MovieCard.jsx
+++ netflix/src/components/MovieCard.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React from 'react';
 
-
-const MovieCard = ({title, image, year}) => {
+const MovieCard = ({ title, image, year }) => {
   return (
-    <div>
-        <img src={image} alt={title}
-        className='w-full h-72 object-cover'/>
-        <div>
-            <h3 className='text-lg font-semibold'>{title}</h3>
-            <p className='text-sm text-gray-400' >{year}</p>
-        </div>
-    </div>
-  )
-}
+    <div className="w-48 bg-white rounded-xl shadow-lg overflow-hidden hover:scale-105 transition duration-300 cursor-pointer">
+  
+      <img
+        src={image}
+        alt={title}
+        className="w-full h-72 object-cover"
+      />
 
-export default MovieCard
+     
+      <div className="p-4 text-center">
+        <h3 className="text-lg font-semibold text-gray-800 truncate">
+          {title}
+        </h3>
+        <p className="text-sm text-gray-500 mt-1">
+          {year}
+        </p>
+      </div>
+
+    </div>
+  );
+};
+
+export default MovieCard;
